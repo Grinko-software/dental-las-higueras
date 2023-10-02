ec704989f6f9783fb15bfc862499daba282f59e7
Nicolas Garcia Santelices <ngarcia1621@gmail.com>
2023-10-02 15:29:36 -0300

    Added cositas

--- app/components/body/sections/about.js
+++ app/components/body/sections/about.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-unused-vars */
-import { Card, CardFooter } from '@nextui-org/react'
-import ScrollAnimation from 'react-animate-on-scroll'
-import Button from '@/ui/button'
+import { Card, CardHeader, CardBody, Divider } from '@nextui-org/react'
 import Image from 'next/image'
-import ShuffleText from '@/ui/text'
-import image2 from '@/assets/images/image2.jpeg'
+import Img2 from '@/assets/images/image2.jpeg'
 import useGlobalStore from '@/store/globalStore'
 import YouTube from 'react-youtube'
 import { useState, useEffect } from 'react'
@@ -29,45 +26,57 @@ export default function About () {
     }, [currentName])
     return (
         <section id='About' className='mt-[2rem] h-full lg:mt-[6rem] flex flex-col items items-center'>
-            <div className="h-auto mx-20 flex flex-row items items-center">
-                <div className=" fade-in-up h-auto pr-0  gap-12 flex flex-row lg:items-stretch lg:pr-10">
-                    <div className="w-full grid gap-[1rem]  text-center lg:text-left lg:justify-left">
-                        <div className='fade-in flex flex-row'>
-                            <h1 className="text-bold fade-in text-xl items-start lg:text-6xl font-[600]" >
+            <div className="w-full h-auto  gap-10 flex flex-col items items-center">
+                <div className="fade-in-up h-auto m-4 flex flex-col lg:flex-row lg:items-center lg:pr-10">
+                    <div className="grid gap-10 lg:w-8/12 text-center lg:text-left lg:justify-center">
+                        <div className='w-full lg:w-8/12 fade-in flex flex-row items-center'>
+                            <h1 className="text-bold fade-in text-3xl  lg:text-6xl font-[600]" >
                                 Atendiendo a nuestros pacientes con
-                                <span className='font-bold fade-in text-xl lg:text-5xl text-primary-600'>
+                                <span className='font-extrabold fade-in text-3xl lg:text-5xl text-primary-600'>
                                     {' ' + currentName.toUpperCase()}
                                 </span>
                             </h1>
                         </div>
-                        <div className='flex flex-row'>
-                            <text className=" text-bold text-2xl text-center lg:text-left lg:justify-lef w-8/12 font-[350]">
+                        <div className='flex flex-col lg:gap-4 lg:flex-row'>
+                            <text className="lg:w-8/12 text-foreground/80 text-bold lg:text-xl lg:text-left lg:justify-lef font-[350]">
                                 Somos una clínica dental comprometida con la salud bucal y la satisfacción de nuestros pacientes. Nuestro equipo altamente calificado de dentistas y especialistas está dedicado a brindar atención dental de alta calidad en un ambiente cálido y acogedor. En nuestra clínica, combinamos la última tecnología con un enfoque personalizado para garantizar que cada paciente reciba el mejor cuidado posible.
                             </text>
-                            <div className="list-disc text-bold text-2xl lg:text-left lg:justify-lef text-center w-4/12 font-[350]">
-                                <text>Especialidades</text>
-                                <li>Odontolgía General</li>
-                                <li>Ortodoncia</li>
-                                <li>Periodoncia</li>
-                                <li>Endodoncia</li>
-                                <li>Cirugías Implantes</li>
-                                <li>Estetica Integral</li>
-                            </div>
+                        </div>
+                        <Divider className='w-8/12'/>
+                        <div className='flex'>
+                            <Card className=" w-full lg:w-8/12 inset-0 bg-gradient-to-r from-primary-500 to-primary-600">
+                                <CardHeader className="flex gap-3 lg:items-start items-center justify-center lg:justify-normal">
+                                    <div className="flex flex-col">
+                                        <p className="text-xl lg:text-5xl font-extrabold ">Especialidades</p>
+                                    </div>
+                                </CardHeader>
+                                <Divider/>
+                                <CardBody>
+                                    <div className="w-full list-disc text-bold text-m lg:text-2xl lg:text-left justify-center text-center space-y-2 font-[350] pressed">
+                                        <li className='text-foreground/80 '>Odontolgía General</li>
+                                        <li className='text-foreground/80 '>Ortodoncia</li>
+                                        <li className='text-foreground/80 '>Periodoncia</li>
+                                        <li className='text-foreground/80 '>Endodoncia</li>
+                                        <li className='text-foreground/80 '>Cirugías Implantes</li>
+                                        <li className='text-foreground/80 '>Estetica Integral</li>
+                                    </div>
+                                </CardBody>
+                                <Divider/>
+                            </Card>
                         </div>
                     </div>
-                    <div className='h-auto flex flex-col items-center lg:items-stretch lg:grid lg:grid-col-2 '>
+                    <div className='h-full flex flex-col items-center lg:items-stretch lg:grid lg:grid-col-2 '>
                         <div className="py-6 px-12 sm:py-12 sm:px-0 fade-in-rigth">
                             <div className="relative py-3 sm:max-w-xl sm:mx-auto">
                                 <div className="flex flex-col items-center justify-center py-2">
                                     <div className="absolute inset-0 bg-gradient-to-r from-indigo-200 to-primary-600 shadow-lg hover:shadow-2xl transform rotate-6 rounded-3xl"></div>
                                     <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-5">
                                         <Image
-                                            src={image2}
-                                            width={'100%'}
-                                            height={'100%'}
+                                            src={Img2}
+                                            width={'120%'}
+                                            height={'120%'}
                                             alt="Logo Dental Las Higueras"
-                                            className='  rounded-3xl'
-
+                                            className='rounded-3xl'
                                         />
                                     </div>
                                 </div>
@@ -75,8 +84,8 @@ export default function About () {
                         </div>
                     </div>
                 </div>
+                <YouTube videoId="dQw4w9WgXcQ"/>
             </div>
-            <YouTube videoId="dQw4w9WgXcQ"/>
         </section>
 
     )
