db63240eea394b8e6c8a27734f51c49e75d692df
NicolasGarciaS <nicolas.garcia.s@etrans.cl>
2023-10-03 15:40:34 -0300

    Added card

--- app/components/body/body.js
+++ app/components/body/body.js
@@ -1,11 +1,13 @@
 'use client'
 import About from './sections/about'
 import Home from './sections/home'
+import Profesionals from './sections/professionals'
 export default function Body () {
     return (
         <main className='flex h-full flex-col mx-10 !scroll-smooth'>
             <Home/>
             <About/>
+            <Profesionals/>
             {/* Agregar secciones acá */}
         </main>
     )
---
+++ app/components/body/sections/professionals.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import { Card, CardHeader, CardBody, Divider } from '@nextui-org/react'
+import Image from 'next/image'
+import Img2 from '@/assets/images/image2.jpeg'
+import useGlobalStore from '@/store/globalStore'
+import YouTube from 'react-youtube'
+import { useState, useEffect } from 'react'
+
+const names = [
+    'Empatia', 'Inovacion', 'Calidad'
+]
+export default function Profesionals () {
+    const { section, setSection } = useGlobalStore(({ section, setSection }) => ({ section, setSection }))
+    const [currentName, setCurrentName] = useState(names[0])
+
+    function setRandomName () {
+        const index = Math.floor(Math.random() * names.length)
+        const newName = names[index]
+        if (newName === currentName) { setRandomName() } else { setCurrentName(newName) }
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            setRandomName()
+        }, 4000)
+    }, [currentName])
+    return (
+        <section id='About' className='mt-[2rem] h-full lg:mt-[6rem] flex flex-col items items-center'>
+            <div className="w-full h-auto  gap-10 flex flex-col items items-center">
+                <div className="fade-in-up h-auto m-4 lg:mx-28 flex flex-col lg:flex-row lg:items-center lg:pr-10">
+
+                </div>
+            </div>
+        </section>
+
+    )
+}
