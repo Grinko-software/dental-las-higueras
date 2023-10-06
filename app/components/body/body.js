e1f7190a069ed2767bda3db3a931aff15685b137
Dionisio Olivares <dionisio.olivares23@gmail.com>
2023-10-06 00:34:11 -0300

    aaaa

--- app/components/body/body.js
+++ app/components/body/body.js
@@ -1,11 +1,15 @@
 'use client'
 import About from './sections/about'
+import Contact from './sections/contact'
 import Home from './sections/home'
+import Shedule from './sections/shedule'
 export default function Body () {
     return (
-        <main className='flex h-full flex-col  !scroll-smooth'>
+        <main className='flex flex-1 flex-col !scroll-smooth'>
             <Home/>
             <About/>
+            <Shedule/>
+            <Contact/>
             {/* Agregar secciones acá */}
         </main>
 
