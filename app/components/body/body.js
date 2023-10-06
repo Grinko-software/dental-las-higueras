8b3d7b1e53f88c2f685c808c098c6a24d39ff1ae
sebastian <ssp013sanchez@gmail.com>
2023-10-06 19:20:09 -0300

    footer

--- app/components/body/body.js
+++ app/components/body/body.js
@@ -1,16 +1,17 @@
 'use client'
+import Footer from '../footer/footer'
 import About from './sections/about'
 import Contact from './sections/contact'
 import Home from './sections/home'
 import Shedule from './sections/shedule'
 export default function Body () {
     return (
-        <main className='flex h-full flex-col mx-10 !scroll-smooth'>
+        <main className='flex h-full flex-col  !scroll-smooth'>
             <Home/>
             <About/>
             <Contact/>
             <Shedule/>
-
+            <Footer/>
             {/* Agregar secciones acá */}
         </main>
     )
--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -1,5 +1,11 @@
 export default function Footer () {
     return (
-        <footer className=" bg-primary-600">Footer</footer>
+
+        <footer className="w-full h-10 flex-row justify-between flex">
+            <div className="my-auto text-xs sm:text-base">© Dental las higueras 2023</div>
+            <div className="my-auto text-xs sm:text-base">Redes</div>
+            <div className="my-auto text-xs sm:text-base"></div>
+        </footer>
+
     )
 }
