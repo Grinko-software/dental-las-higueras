05c56ff8137abc3899bbbd5dfa9f67939d912696
sebastian <ssp013sanchez@gmail.com>
2023-10-06 00:22:10 -0300

    added

--- app/components/body/body.js
+++ app/components/body/body.js
@@ -4,6 +4,10 @@ export default function Body () {
     return (
         <main className='flex h-full flex-col  !scroll-smooth'>
             <Home/>
+            <Home/>
+            <Home/>
+            <Home/>
+            <Home/>
             {/* Agregar secciones acá */}
         </main>
 
