88733db1fc0249bf5627ad460ff79bb3389983e4
Nicolas Garcia Santelices <ngarcia1621@gmail.com>
2023-10-08 17:44:19 -0300

    Added profesionals

--- app/components/body/sections/professionals.js
+++ app/components/body/sections/professionals.js
@@ -9,27 +9,93 @@ import Carroussel from '../../carussel'
 import { v4 as uuidv4 } from 'uuid'
 import Waves from './waves'
 
+const ProfesionalsArray = [
+
+    {
+        srcRute: '../../../../assets/images/juanito.png',
+        name: 'Dr. Juan Carlos Benavides Solarte',
+        specialty: 'Cirujano Dentista',
+        specialist: 'Ortodoncia y Ortopedia Bucomaxilofacial',
+        university: 'Universidad Antonio Narino, Colombia Reconocido por Minist. Relaciones Exteriores el 22 de Octubre de 2009',
+        experience: [
+            'Cirujano Dentista en el área privada con más de 12 años de experiencia',
+            'Especialidad de Ortodoncia y O.M.B, Universidad Católica de Uruguay',
+            'Maestría en Anclaje Esqueletal Instituto Mondelli de Ortodoncia, Brasil',
+            'Residencia en Microtornillos Extraalveolares',
+            'Certificación en Invisaling',
+            'Actualización en ortodoncia y vías aéreas Universidad de New York'
+        ]
+
+    },
+    {
+        srcRute: '../../../../assets/images/juanito.png',
+        name: 'Dr. Rodolfo Zamora',
+        specialty: 'Especialista titulado',
+        specialist: ' -',
+        university: 'certificado por la Universidad del Desarrollo y la Clínica Alemana de Santiago.  Titulado con alto honor de la Universidad Mayor de Santiago.',
+        experience: [
+            'Docente Titular de pregrado del Departamento de Cirugia Oral.',
+            'Miembro de la Sociedad De implantologìa Oral de Chile',
+            'Ex jefe de programa de Vichuquen de odontología y de programas'
+        ]
+
+    },
+    {
+        srcRute: '../../../../assets/images/juanito.png',
+        name: 'Dr. José Ignacio Iturra Marín',
+        specialty: 'Cirujano Dentista',
+        specialist: '-',
+        university: 'Universidad de Antofagasta (2021)',
+        experience: [
+            'Cirujano Dentista con experiencia en atención primaria y urgencias (Hospital Carlos Cisternas, Calama)',
+            'Desarrollo y Jefe de unidad Móvil en “Programa de salud Bucal” de JUNAEB y Odontopediatría (2022 a 2023)',
+            'Desarrollo y Jefe de unidad Móvil en “Programa de salud Bucal” de JUNAEB y Odontopediatría (2022 a 2023)'
+
+        ]
+
+    },
+
+    {
+        srcRute: '../../../../assets/images/juanito.png',
+        name: 'Dra. Nataly Veas Cortés',
+        specialty: 'Cirujano Dentista',
+        specialist: 'Periodoncia',
+        university: 'Licenciado en Ciencias Odontológicas Universidad de Antofagasta',
+        experience: [
+            'Cirujano Dentista en el área privada con más de 5 años de experiencia',
+            'Especialidad de Periodoncia, Universidad del Desarrollo',
+            'Diplomado Ortodoncia-Periodoncia, Universidad del Desarrollo'
+        ]
+
+    }
+
+]
+
 const slides = [
     // eslint-disable-next-line react/jsx-key
     {
         key: uuidv4(),
-        content: <ProfesionalsCard alt="0"/>
+        content: <ProfesionalsCard alt="0" Profesional={ProfesionalsArray[0]}/>
     },
     {
         key: uuidv4(),
-        content: <ProfesionalsCard alt="1"/>
+        content: <ProfesionalsCard alt="1" Profesional={ProfesionalsArray[1]}/>
     },
     {
         key: uuidv4(),
-        content: <ProfesionalsCard alt="2"/>
+        content: <ProfesionalsCard alt="2" Profesional={ProfesionalsArray[2]}/>
     },
     {
         key: uuidv4(),
-        content: <ProfesionalsCard alt="3"/>
+        content: <ProfesionalsCard alt="3" Profesional={ProfesionalsArray[3]}/>
     },
     {
         key: uuidv4(),
-        content: <ProfesionalsCard alt="4"/>
+        content: <ProfesionalsCard alt="4" Profesional={ProfesionalsArray[4]}/>
+    },
+    {
+        key: uuidv4(),
+        content: <ProfesionalsCard alt="5" Profesional={ProfesionalsArray[5]}/>
     }
 
 ]
--- app/components/card.js
+++ app/components/card.js
@@ -1,13 +1,12 @@
 import { React, useState, useEffect } from 'react'
 import Image from 'next/image'
-import image1 from '@/assets/images/juanito2.png'
 import { Divider, Button, Card } from '@nextui-org/react'
 import ReactCardFlip from 'react-card-flip'
 
 export default function ProfesionalsCard (props) {
+    const { Profesional } = props
     const [isFlipped, setIsFlipped] = useState(false)
     const flipCard = () => setIsFlipped(!isFlipped)
-
     useEffect(() => {
         setInterval(() => {
             if (isFlipped) {
@@ -16,62 +15,64 @@ export default function ProfesionalsCard (props) {
         }, 10000)
     }, [isFlipped])
     return (
-
-        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-            <Card className="backdrop-blur-xl shadow-xl bg-primary-600/2 border rounded-2xl border-gray-200 flex flex-col items items-center ">
-                <div className="w-[17rem] h-[26rem] sm:w-[27rem] sm:h-[40rem] flex-col sm:mx-10 text-white ">
-                    <div className="absolute inset-0 bg-white rounded-2xl flex justify-center items-center flex-row z-20">
-                        <div className="flex flex-col items-center gap-4 sm:gap-10 ">
-                            <div className='flex flex-col items-center gap-5'>
-                                <div className='w-4/12 sm:w-6/12 '>
-                                    <Image
-                                        imgRef={image1}
-                                        src={image1}
-                                        alt="Logo Dental Las Higueras"
-                                        width={'100%'}
-                                        height={'100%'}
-                                        className='rounded-full shadow-lg'
-                                    />
+        <div>
+            {Profesional != null
+                ? <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+                    <Card className="backdrop-blur-xl shadow-xl bg-primary-600/2 border rounded-2xl border-gray-200 flex flex-col items items-center ">
+                        <div className="w-[17rem] h-[26rem] sm:w-[27rem] sm:h-[40rem] flex-col sm:mx-10 text-white ">
+                            <div className="absolute inset-0 bg-white rounded-2xl flex justify-center items-center flex-row z-20">
+                                <div className="flex flex-col items-center gap-4 sm:gap-10 ">
+                                    <div className='flex flex-col items-center gap-5'>
+                                        <div className='w-4/12 sm:w-6/12 '>
+                                            <Image
+                                                /* src={Profesional ? Profesional.srcRute : null} */
+                                                width={'100%'}
+                                                height={'100%'}
+                                                className='rounded-full shadow-lg'
+                                            />
+                                        </div>
+                                        <h5 className="text-xl sm:text-3xl text-center font-medium text-gray-900">{Profesional.name}</h5>
+                                        <span className="text-xl sm:text-2xl font-medium text-gray-500 dark:text-gray-400 ">{Profesional.specialty}</span>
+                                        <Divider className='w-8/12 bg-slate-500 rounded-xl'></Divider>
+                                    </div>
+                                    <div className='flex flex-col items-center  gap-1'>
+                                        <span className="text-sm sm:text-xl  text-gray-800 dark:text-gray-400 ">Especialidad</span>
+                                        <span className="text-sm sm:text-xl  text-gray-500 dark:text-gray-400 mb-10">{Profesional.specialist}</span>
+                                        <Button className="bg-primary-600 font-bold text-white font-2xl" variant="shadow" onClick={flipCard}>Experiencia Laboral</Button>
+                                    </div>
                                 </div>
-                                <h5 className="text-xl sm:text-3xl text-center font-medium text-gray-900">Dr. Juan Carlos Benavides Solarte</h5>
-                                <span className="text-xl sm:text-2xl font-medium text-gray-500 dark:text-gray-400 ">Cirujano Dentista</span>
-                                <Divider className='w-8/12 bg-slate-500 rounded-xl'></Divider>
-                            </div>
-                            <div className='flex flex-col items-center  gap-1'>
-                                <span className="text-sm sm:text-xl  text-gray-800 dark:text-gray-400 ">Especialidad</span>
-                                <span className="text-sm sm:text-xl  text-gray-500 dark:text-gray-400 mb-10">Ortodoncia y Ortopedia Bucomaxilofacial</span>
-                                <Button className="bg-primary-600 font-bold text-white font-2xl" variant="shadow" onClick={flipCard}>Experiencia Laboral</Button>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </Card>
-            <Card >
-                <div className="w-[17rem] h-[26rem] sm:w-[27rem] sm:h-[40rem]  bg-slate-800  text-white ">
-                    <div className='absolute inset-0 rounded-2xl flex flex-col justify-center items-center z-20 gap-2 sm:gap-10 mx-2 sm:mx-12 '>
-                        <div className='flex flex-col items-center gap-4 sm:gap-10'>
-                            <div className="flex flex-col gap-1">
-                                <h3 className="font-bold text-white text-foreground/90 text-lg sm:text-2xl">Dr. Juan Carlos Benavides Solarte</h3>
-                                <Divider className='bg-primary-600/10 h-0.5 rounded-sm'></Divider>
-                                <p className="text-xs font-light ">Universidad Antonio Narino, Colombia Reconocido por Minist. Relaciones Exteriores el 22 de Octubre de 2009</p>
+                    </Card>
+                    <Card >
+                        <div className="w-[17rem] h-[26rem] sm:w-[27rem] sm:h-[40rem]  bg-slate-800  text-white ">
+                            <div className='absolute inset-0 rounded-2xl flex flex-col justify-center items-center z-20 gap-2 sm:gap-10 mx-2 sm:mx-12 '>
+                                <div className='flex flex-col items-center gap-4 sm:gap-10'>
+                                    <div className="flex flex-col gap-1">
+                                        <h3 className="font-bold text-white text-foreground/90 text-lg sm:text-2xl">{Profesional.name}</h3>
+                                        <Divider className='bg-primary-600/10 h-0.5 rounded-sm'></Divider>
+                                        <p className="text-xs font-light ">{Profesional.university}</p>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col items-center  gap-2">
+                                    <p className="text-xl font-bold text-white text-foreground/80">Experiencia Laboral</p>
+                                    <Divider className='w-10/12'></Divider>
+                                    <div className="flex flex-col justify-between list-disc gap-1 sm:gap-4">
+                                        {Profesional.experience
+                                            ? Profesional.experience.map(
+                                                (item) => (
+                                                    <li key={item} className=" text-xs sm:text-sm font-light">{item}</li>
+                                                )
+                                            )
+                                            : null}
+                                    </div>
+                                </div>
+                                <Button className="bg-primary-600 font-bold text-white font-2xl" variant="shadow" onClick={flipCard}>Información General</Button>
                             </div>
                         </div>
-                        <div className="flex flex-col items-center  gap-2">
-                            <p className="text-xl font-bold text-white text-foreground/80">Experiencia Laboral</p>
-                            <Divider className='w-10/12'></Divider>
-                            <div className="flex flex-col justify-between list-disc gap-1 sm:gap-4">
-                                <li className=" text-xs sm:text-sm font-light">Cirujano Dentista en el área privada con más de 12 años de experiencia</li>
-                                <li className=" text-xs sm:text-sm font-light">Especialidad de Ortodoncia y O.M.B, Universidad Católica de Uruguay</li>
-                                <li className=" text-xs sm:text-sm font-light">Maestría en Anclaje Esqueletal Instituto Mondelli de Ortodoncia, Brasil</li>
-                                <li className=" text-xs sm:text-sm font-light">Residencia en Microtornillos Extraalveolares</li>
-                                <li className=" text-xs sm:text-sm font-light">Certificación en Invisaling</li>
-                                <li className=" text-xs sm:text-sm font-light">actualización en ortodoncia y vías aéreas Universidad de New York</li>
-                            </div>
-                        </div>
-                        <Button className="bg-primary-600 font-bold text-white font-2xl" variant="shadow" onClick={flipCard}>Información General</Button>
-                    </div>
-                </div>
-            </Card>
-        </ReactCardFlip>
+                    </Card>
+                </ReactCardFlip>
+                : <div></div>}
+        </div>
     )
 }
