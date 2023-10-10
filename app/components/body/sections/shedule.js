dcb7b00dae4981991d1a2c04e2f67910fb679fe9
sebastian <ssp013sanchez@gmail.com>
2023-10-10 00:50:17 -0300

    fixed providers

--- app/components/body/sections/shedule.js
+++ app/components/body/sections/shedule.js
@@ -3,7 +3,7 @@ export default function Shedule () {
         <section id='Agendar' className='flex flex-1 flex-col items items-center'>
             <div className="h-full mx-5 flex flex-col items items-center">
                 <div className='fade-in flex flex-row py-5'>
-                    <h5 className="text-bold fade-in text-3xl items-start lg:text-6xl font-[600]" >
+                    <h5 className="text-bold fade-in text-3xl items-start lg:text-6xl font-[600] text-primary-700" >
                         Agenda con nosotros
                     </h5>
                 </div>
--- app/providers.js
+++ app/providers.js
@@ -3,7 +3,7 @@ import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider } from 'next-themes'
 export function Providers ({ children }) {
     return (
-        <ThemeProvider>
+        <ThemeProvider attribute='c'>
             <NextUIProvider>
                 {children}
             </NextUIProvider>
