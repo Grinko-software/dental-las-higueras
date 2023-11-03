0001e94838e62a1138519a7d2709776f4068e350
sebastian <ssp013sanchez@gmail.com>
2023-11-02 21:08:46 -0300

    added tag manager

--- app/components/body/body.js
+++ app/components/body/body.js
@@ -5,7 +5,24 @@ import Home from './sections/home'
 import Shedule from './sections/shedule'
 import Profesionals from './sections/professionals'
 import Waves from './sections/waves'
+import { useEffect } from 'react'
+
+export const initializeGoogleTagManager = () => {
+    (function (w, d, s, l, i) {
+        w[l] = w[l] || []; w[l].push({
+            'gtm.start':
+new Date().getTime(),
+            event: 'gtm.js'
+        }); const f = d.getElementsByTagName(s)[0]
+        const j = d.createElement(s); const dl = l !== 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f)
+    })(window, document, 'script', 'dataLayer', 'GTM-5FJWB7K9')
+}
+
 export default function Body () {
+    useEffect(() => {
+        initializeGoogleTagManager()
+    }, [])
     return (
         <main className='flex h-full flex-col !scroll-smooth bg-primary-100'>
             <Home/>
